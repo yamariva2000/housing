50852f98bcad1e745d5ddc5fa573dd0099dea320
kel <yamariva_2000@yahoo.com>
2017-05-08 18:19:17 -0700

    Initial commit

---
+++ housing.py
@@ -0,0 +1,196 @@
+from datetime import datetime
+import matplotlib.cm as cm
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import seaborn as sns
+from sklearn.cluster import KMeans
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.preprocessing import StandardScaler
+
+"""Index(['id', 'date', 'price', 'bedrooms', 'bathrooms', 'sqft_living',
+       'sqft_lot', 'floors', 'waterfront', 'view', 'condition', 'grade',
+       'sqft_above', 'sqft_basement', 'yr_built', 'yr_renovated', 'zipcode',
+       'lat', 'long', 'sqft_living15', 'sqft_lot15'],
+      dtype='object')"""
+
+
+class data():
+    def __init__(self):
+        self.centers = centers
+        self.xm = xm
+        df = pd.read_csv('/home/kel/housing/kc_house_data.csv - kc_house_data.csv', parse_dates=True)
+
+        df.date = df.date.apply(lambda x: datetime.strptime(x[:-7], "%Y%m%d"))
+        df['age'] = df.date.dt.year - df.yr_built
+        df['time'] = (df.date - min(df.date)).astype(int)
+
+        cond = df.yr_renovated == 0
+        df.yr_renovated[cond] = df.yr_built
+
+        df['renovation_recency'] = df.date.dt.year - df.yr_renovated
+        self.df = df
+
+    def plots(self):
+        df = self.df
+
+        df['price_ft'] = df.price / df.sqft_living
+        hists = ['waterfront', 'view']
+        f = plt.figure(1, figsize=(20, 15))
+        for i, h in enumerate(hists, start=1):
+            ax = f.add_subplot(2, 1, i)
+            sns.distplot(df['price_ft'][df[h] == 1],
+                         label='average {} ${:,.0f}/sft'.format(h, df['price_ft'][df[h] == 1].mean()), ax=ax)
+
+            sns.distplot(df['price_ft'][df[h] == 0],
+                         label='average no {} ${:,.0f}/sft'.format(h, df['price_ft'][df[h] == 0].mean()), ax=ax)
+            ax.set_title('{} $/sft'.format(h), fontsize=20)
+            ax.legend(fontsize=15)
+
+        numberfeatures = ['grade', 'sqft_living', 'sqft_lot', 'bedrooms', 'bathrooms', 'renovation_recency']
+
+        f = plt.figure(2, figsize=(20, 15))
+
+        df = df
+        for i, feature in enumerate(numberfeatures, start=1):
+            ax = f.add_subplot(2, 3, i)
+            ax.scatter(df[feature], df['price'])
+            ax.set_title(feature, fontsize=20)
+            # ax.legend(fontsize=15)
+
+        plt.show()
+
+    def cluster(self):
+        mdf = self.model_data()
+
+        y = mdf.pop('price')
+        x = mdf
+        xm = x.drop(['month'], axis=1)
+
+        scaler = StandardScaler()
+        scaler2 = StandardScaler()
+        normx = scaler.fit_transform(xm)
+        normy = scaler2.fit_transform(y)
+        n_clusters = 10
+        k = KMeans(n_clusters=n_clusters)
+        predictions = k.fit_predict(normx, normy)
+
+        y = pd.concat([pd.Series(predictions, name='group'), y], axis=1)
+
+        price_groups = y.groupby('group')['price'].agg(['mean', 'median', 'count'])
+
+        centers = scaler.inverse_transform(k.cluster_centers_)
+        centers = pd.DataFrame(centers, columns=xm.columns)
+
+        centers = pd.concat((centers, price_groups), axis=1)
+
+        centers.sort_values(by=['median'], inplace=True)
+
+        xm = pd.concat([xm, pd.Series(predictions, name='group')], axis=1)
+        return xm['group']
+
+    def map(self):
+        self.cluster()
+        centers = self.centers
+        xm = self.xm
+        max_median = np.max(centers['median'].values)
+
+        for i in centers.index:
+            group = xm[xm.group == i]
+            size = min(1000, len(group))
+
+            group = group.sample(size)
+
+            plt.scatter(group.long, group.lat,
+                        label='n={:n}  ${:0f}'.format(centers.loc[i]['count'], centers.loc[i]['median']),
+                        c=cm.Reds(centers.loc[i]['median'] / max_median))
+
+        # plt.scatter(centers['long'],centers['lat'],label='Centers',marker='^',s=200)
+
+        plt.xlabel('Longitude', size=25)
+        plt.ylabel('Latitude', size=25)
+        plt.legend(fontsize=12)
+        plt.title('Seattle-Bellvue Real Estate', fontsize=30)
+        plt.show()
+
+    def model_data(self):
+
+        mdf = self.df
+        # mdf.drop(['id','date','lat','long'],axis=1,inplace=True)
+        mdf.drop(['id'], axis=1, inplace=True)
+        mdf['month'] = mdf.date.dt.month
+
+        mdf.index = mdf.date
+        # mdf.drop(['date','yr_renovated','yr_built','zipcode','sqft_above','floors','month','time','view','age','sqft_living15','sqft_basement','condition','bathrooms','bedrooms','renovation_recency','sqft_lot','sqft_lot15'],axis=1,inplace=True)
+        mdf.drop(['date', 'yr_renovated', 'yr_built', 'zipcode'], axis=1, inplace=True)
+
+        return mdf
+
+    def same_sales(self):
+        mdf = self.model_data()
+        mdf['date'] = mdf.index
+
+        agg = mdf.groupby(['long', 'lat'])['price'].count().reset_index()
+        agg.rename(columns={'price': 'count'}, inplace=True)
+
+        mask = agg['count'] > 1
+
+        merge = pd.merge(mdf, agg[mask], on=['long', 'lat'])
+        merge.sort_values(by=['long', 'lat', 'date'], inplace=True)
+
+        print(merge[['long', 'lat', 'date', 'renovation_recency', 'price']])
+
+    def estimator(self):
+
+        # groups=self.cluster()
+
+        mdf = self.model_data()
+
+        y = mdf.pop('price')
+        # y.index=groups
+        x = mdf
+
+        dt = datetime(2015, 1, 1)
+
+        xtrain, xtest, ytrain, ytest = x[x.index < dt], x[x.index >= dt], y[y.index < dt], y[y.index >= dt]
+
+        model = RandomForestRegressor(n_estimators=20, n_jobs=4, random_state=1)
+
+        model.fit(xtrain, ytrain)
+
+        print(model.score(xtest, ytest))
+        print(xtrain.columns[np.argsort(model.feature_importances_)[::-1]])
+
+        ytest_predict = pd.Series(model.predict(xtest), index=ytest.index, name='ypredict')
+
+        ycombined = pd.concat([ytest, ytest_predict], axis=1)
+
+        ycombined.rename(columns={'price': 'actual_price'}, inplace=True)
+
+        ycombined['resid'] = ycombined['ypredict'] - ycombined['actual_price']
+        # ycombined['price_buckets']=pd.qcut(ycombined.actual_price,q=5)
+        ycombined['price_buckets'] = pd.cut(ycombined.actual_price,
+                                            bins=[0, 300000, 500000, 700000, 1000000, 6000000])
+
+        percent = .1
+        ycombined['within'] = ((ycombined.actual_price < ycombined.ypredict * (1 + percent)) & (
+            ycombined.actual_price > ycombined.ypredict * (1 - percent)))
+
+        print(ycombined.groupby('price_buckets')['within'].agg({'percent': lambda x: sum(x) / len(x), 'count': len}))
+        print(ycombined.head())
+
+        exit()
+        sample = ycombined.sample(100)
+        # sample.sort_index(inplace=True)
+        # plt.plot(sample.index,sample.actual_price,label='actual')
+        # plt.plot(sample.index,sample.ypredict,label='est')
+        # plt.legend()
+        sns.violinplot(ycombined.index, ycombined.resid, ls='', marker='o')
+        plt.show()
+
+
+d = data()
+# d.plots()
+# d.map()
+d.estimator()
+# d.same_sales()
---
+++ readme.md
@@ -0,0 +1,60 @@
+##Real Estate Case Study
+###Kel Yip
+
+In this dataset, there are approximately 21,000 recorded home sales ranging in price from  $83,000 to over $7 million.  Without looking at the zip codes, and plotting the longitude and latitude for each sale results in a map-like plot below (Figure 1):  
+
+![Fig 1](seattle.png)
+
+This plot resembles the Seattle area with respect to the open areas that are inlets of water.  Home sales were classified into 10 cluster based on feature characteristics.  The darker groupings represent more expensive homes.  As one can see from the coloration, waterfront homes and homes near water.  Also homes that are at higher elevation with views to the water also are more expensive.    Figure 2 below shows waterfront and view home histograms of price per square feet.
+
+
+![Fig 2](water.png)
+
+On both a square footage and absolute price basis, waterfront homes command significantly higher prices.  The premium for views is less on a square foot basis, but view homes tend to be larger in general leading to higher overall price.
+
+Below are scatter plots of home price vs various features (Figure 3).
+
+![Fig 3](scatters.png)
+
+
+For quantitative variables, home price is positively correlated with living space, number of bathrooms, and grade.  Further clarification on what grade is necessary.  If one interprets grade as the steepness of the lot, this would imply that houses on edges of hillsides are more expensive than those on flat lots.  In real estate, location is always mentioned as an important determinant of home value.  Most likely longitude and latitude are very important in the determination of home price.
+
+A random forest regressor was used to predict home prices from various feature vectors.  The first model used the following variables:
+
+Index(['sqft_living', 'grade', 'lat', 'long', 'waterfront', 'sqft_living15',
+       'age', 'sqft_above', 'sqft_lot15', 'sqft_lot', 'view',
+       'renovation_recency', 'bathrooms', 'time', 'sqft_basement', 'bedrooms',
+       'condition', 'month', 'floors'],
+      dtype='object')
+Several variables were constructed from the dataset, including renovation_recency -the time between date of home sale and year of last renovation, age - the time between date built and home sale date, and time - the number of days from the home sale date to the beginning of the dataset,
+
+The training dataset included home sales up to January 1, 2015, and the test set after this date.  This resulted in about 14,000 training points and 7000 test points.
+
+The resulting score was an R-squared of 0.86 and increase of 35 percentage points above the mean.  The model determined the most important features as listed above where living space, grade, latitude, longitude, and waterfront.
+
+The second model included a minimal set of variable to achieve a score of .85:
+Index(['sqft_living', 'grade', 'lat', 'long', 'waterfront'], dtype='object').  
+
+The final test was to provide a confidence level for a customer with our prediction model.  For the each observation in the test dataset, the model determined whether the actual sales price was within 10% of the model estimate.  The percent within the band divided by the number of observations is the expected probability the the customer’s selling price is within 10% of the model.
+
+<table>
+
+<th>price_buckets</th><th>percent</th><th>count</th>
+<tr><td>(0, 300000]</td><td>0.44</td><td>1450</td></tr>
+
+<tr><td>(300000, 500000]</td><td>    0.534082</td>   <td>2582</td></tr>
+
+<tr><td>(500000, 700000]</td><td>    0.487852 </td>  </td><td>1564</td></tr>
+<tr><td>(700000, 1000000]</td><td>   0.458655</td>    <td>907</td></tr>
+<tr><td>(1000000, 6000000]</td><td>  0.289308</td>    <td>477</td></tr>
+
+</table>
+
+I appears that the model provides higher confidence for homes that are between 300,000 and 500,000, with lower confidence for homes over $1 million.  This makes sense because homes in the upper price range have lower transaction volume and more volatile pricing.
+
+Further work would be to determine if there are any time-related factors for home pricing.  Our variable time was not very predictive in this model.  The analysis found instances of same sale homes over time.  This information could be used to improve predictions especially for homes with prior sales information.  
+
+
+
+
+
